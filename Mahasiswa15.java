1ac86eeed04ecc9fac742e5110dc6374211c4278
nazwazhrr <nazwaazahra883@gmail.com>
2025-05-03 15:13:14 +0700

    ASD 9

---
+++ Mahasiswa15.java
@@ -0,0 +1,27 @@
+public class Mahasiswa15 {
+    String nama;
+    String nim;
+    String kelas;
+    int nilai;
+
+    public Mahasiswa15() {
+    }
+
+    Mahasiswa15(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim  = nim;
+        this.kelas = kelas;
+        this.nilai = -1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+
+    public void tampil() {
+        System.out.println("Nama : " +  nama);
+        System.out.println("NIM : " + nim);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("Nilai : " + nilai);
+    }
+}
---
+++ MahasiswaDemo15.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+ public class MahasiswaDemo15 {
+     public static void main(String[] args) {
+         Scanner input15 = new Scanner(System.in);
+         StackTugasMahasiswa15 stack = new StackTugasMahasiswa15(5);
+         int pilih;
+ 
+        do {
+         System.out.println("\nMenu :");
+         System.out.println("1. Mengumpulkan Tugas");
+         System.out.println("2. Menilai Tugas");
+         System.out.println("3. Melihat Tugas Teratas");
+         System.out.println("4. Melihat Daftar Tugas");
+         System.out.print("Pilih: ");
+         pilih = input15.nextInt();
+         input15.nextLine();
+         switch (pilih) {
+             case 1 :
+                 System.out.print("Nama: ");
+                 String nama = input15.nextLine();
+                 System.out.print("Nim : ");
+                 String nim = input15.nextLine();
+                 System.out.print("Kelas: ");
+                 String kelas = input15.nextLine();
+                 Mahasiswa15 mhs = new Mahasiswa15(nama, nim, kelas);
+                 stack.push(mhs);
+                 System.out.printf("Tugas %s berhasil dikumpulkan\n", mhs.nama);
+                 break;
+             case 2:
+                 Mahasiswa15 dinilai = stack.pop();
+                 if (dinilai != null) {
+                     System.out.println("Menilai tugas dari " + dinilai.nama);
+                     System.out.print("Masukkan nilai (0-100): ");
+                     int nilai = input15.nextInt();
+                     dinilai.tugasDinilai(nilai);
+                     System.out.printf("Nilai Tugas %s adalah %d\n", dinilai.nama, nilai);
+                 }
+                 break;
+             case 3:
+                 Mahasiswa15 lihat = stack.peek();
+                 if (lihat != null) {
+                     System.out.println("Tugas terakhir dikumpulkan oleh " + lihat.nama);
+                 }
+                 break;
+             case 4:
+                 System.out.println("Daftar semua tugas");
+                 System.out.println("Nama\tNIM\tKelas");
+                 stack.print();
+                 break;   
+             default:
+                 System.out.println("Pilihan tidak valid.");
+             }
+         } while (pilih >= 1 && pilih <= 4);
+      }
+ }
