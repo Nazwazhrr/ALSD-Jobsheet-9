61a74075ce3d139cbd7bcd96ba1c270c024cf471
nazwazhrr <nazwaazahra883@gmail.com>
2025-05-03 19:02:03 +0700

    modifikasi

--- MahasiswaDemo15.java
+++ MahasiswaDemo15.java
@@ -70,13 +70,13 @@ import java.util.Scanner;
 
       public static String konversiDesimalKeBiner(int nilai) {
         StackKonversi15 stack = new StackKonversi15();
-        while (nilai > 0) {
+        while (nilai != 0) {
             int sisa = nilai % 2;
             stack.push(sisa);
             nilai = nilai / 2;
         }
     
-        String biner = "";
+        String biner = new String();
         while (!stack.isEmpty()) {
             biner += stack.pop();
         }
